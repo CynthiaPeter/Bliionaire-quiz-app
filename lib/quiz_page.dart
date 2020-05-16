99705e99d36e718e7a4adcfd216fbdc201b07077
Cynthia Peter <cynthiapeterpearl@gmail.com>
2020-05-16 07:59:11 +0100

    One widget per day is better than nothing, the answer tickers added

--- lib/quiz_page.dart
+++ lib/quiz_page.dart
@@ -15,40 +15,73 @@ class _QuizPageState extends State<QuizPage> {
           children: <Widget>[
             Container(
               height: MediaQuery.of(context).size.height * 0.9,
-              child: Column(crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('Slumdog Millionaire', 
-                style: TextStyle(fontWeight: FontWeight.w400,
-                fontSize: 20,
-                color: Colors.white.withOpacity(0.6),),),
-                 SizedBox(
-                  height: 10,
-                ),
-                Row(crossAxisAlignment: CrossAxisAlignment.end,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                   Text(
-                      'Question  ',
-                      style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontSize: 20,
-                          color: Colors.white.withOpacity(0.8)),
+                  Text(
+                    'Slumdog Millionaire',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 15,
+                      color: Colors.white.withOpacity(0.3),
                     ),
-                    Text(
-                      '06',
-                      style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontSize: 23,
-                          color: Colors.white.withOpacity(0.7)),
-                    ),
-                     Text(
-                      '/20',
-                      style: TextStyle(
-                          fontWeight: FontWeight.w100,
-                          fontSize: 20,
-                          color: Colors.white.withOpacity(0.5)),
-                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      Text(
+                        'Question  ',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 20,
+                            color: Colors.white.withOpacity(0.8)),
+                      ),
+                      Text(
+                        '06',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 23,
+                            color: Colors.white.withOpacity(0.7)),
+                      ),
+                      Text(
+                        '/20',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w100,
+                            fontSize: 20,
+                            color: Colors.white.withOpacity(0.5)),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height:20),
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.check, color: Colors.greenAccent, size: 14,),
+                      Icon(Icons.check, color: Colors.greenAccent, size: 14, ),
+                      Icon(Icons.check, color: Colors.red, size: 14,),
+                      Icon(Icons.check, color: Colors.greenAccent, size: 14,),
+                      Icon(Icons.check, color: Colors.greenAccent, size: 14,),
+                      Icon(Icons.check, color: Colors.red, size: 14,),
+                      Icon(Icons.check, color: Colors.greenAccent, size: 14,),
+                      Icon(Icons.check, color: Colors.red, size: 14,),
+                      Icon(Icons.check, color: Colors.greenAccent, size: 14,),
+                      Icon(Icons.check, color: Colors.greenAccent, size: 14,),
+                      Icon(Icons.check, color: Colors.greenAccent, size: 14,),
+                      Icon(Icons.check, color: Colors.greenAccent, size: 14,),
+                      Icon(Icons.check, color: Colors.greenAccent, size: 14,),
+                      Icon(Icons.check, color: Colors.red, size: 14,),
+                      Icon(Icons.check, color: Colors.greenAccent, size: 14,),
+                      Icon(Icons.check, color: Colors.greenAccent, size: 14,),
+                      Icon(Icons.check, color: Colors.red, size: 14,),
+                      Icon(Icons.check, color: Colors.greenAccent, size: 14,),
+                      Icon(Icons.check, color: Colors.greenAccent, size: 14,),
+                      Icon(Icons.check, color: Colors.greenAccent, size: 14,),
+                    ],
+                  )
                 ],
-               ), ],),
+              ),
             )
           ],
         ),
